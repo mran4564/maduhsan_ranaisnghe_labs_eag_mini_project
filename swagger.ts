b4d67f9d92282bb797d6721c75d09e5656b95320
mran4564 <Madushan.Ranasinghe@sysco.com>
2023-11-22 22:51:50 +0530

    init project

---
+++ swagger.ts
@@ -0,0 +1,18 @@
+export const swaggerDocument = {
+    openapi: "3.0.1",
+    info: {
+      version: "1.0.0",
+      title: "APIs Documentation",
+      description: "B2B Portal BFF",
+      termsOfService: "",
+      contact: {
+        name: "Madushan Ranasinghe",
+        email: "",
+        url: "",
+      },
+      license: {
+        name: "",
+        url: "",
+      },
+    },
+  };
