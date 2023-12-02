dde8b8c0319a7f9066e167cb3d8e7f4544cba29e
mran4564 <Madushan.Ranasinghe@sysco.com>
2023-12-03 00:33:16 +0530

    (auth): add authentication configuration to redux store

--- apps/auth/src/app/app.tsx
+++ apps/auth/src/app/app.tsx
@@ -1,36 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import Login from '../redux/features/auth/login';
+import Login from '../components/Login.component';
 import { Route, Routes } from 'react-router-dom';
-import RequireAuth from '../redux/features/auth/requireAuth';
-import Welcome from '../redux/features/auth/welcome';
-import Layout from '../components/layout.component';
-import { setCredentials } from '../redux/features/auth/authSlice';
-import { useDispatch } from 'react-redux';
 
 export function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const userData = sessionStorage.getItem('userData');
-    const idToken = sessionStorage.getItem('idToken');
-    const refreshToken = sessionStorage.getItem('refreshToken');
-    if (userData !== undefined && userData !== null) {
-      const { userId, userRole } = JSON.parse(userData);
-      dispatch(setCredentials({ userId, userRole, idToken, refreshToken }));
-    }
-  }, [dispatch]);
-
   return (
     <ChakraProvider>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* public routes */}
-          <Route path="login" element={<Login />} />
-          {/* protected routes */}
-          <Route element={<RequireAuth />}>
-            <Route path="welcome" element={<Welcome />} />
-          </Route>
-        </Route>
+        <Route path="login" element={<Login />} />
       </Routes>
     </ChakraProvider>
   );
--- apps/auth/src/bootstrap.tsx
+++ apps/auth/src/bootstrap.tsx
@@ -2,19 +2,18 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
-import { BrowserRouter, Router } from 'react-router-dom';
-import { store } from '../src/redux/store';
-import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { StoreProvider } from 'global-store/Module';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <Provider store={store}>
+    <StoreProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
+    </StoreProvider>
   </StrictMode>
 );
