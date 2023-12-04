b1bfe74eaad7a07ad2347dfe00652bf06d00dbe7
mran4564 <Madushan.Ranasinghe@sysco.com>
2023-12-04 23:49:39 +0530

    (batch-service) init

---
+++ batch-service/src/main/java/org/b2b_system/batch/BatchServiceApplication.java
@@ -0,0 +1,9 @@
+package org.b2b_system.batch;
+
+import org.springframework.boot.SpringApplication;
+
+public class BatchServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BatchServiceApplication.class, args);
+    }
+}
