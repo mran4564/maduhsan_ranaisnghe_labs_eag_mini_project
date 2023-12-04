756a399df9605c51d711a4c724a5614afa776769
mran4564 <Madushan.Ranasinghe@sysco.com>
2023-12-05 00:05:31 +0530

    (batch-service) add application properties

--- batch-service/src/main/java/org/b2b_system/batch/BatchServiceApplication.java
+++ batch-service/src/main/java/org/b2b_system/batch/BatchServiceApplication.java
@@ -1,7 +1,9 @@
 package org.b2b_system.batch;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BatchServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BatchServiceApplication.class, args);
