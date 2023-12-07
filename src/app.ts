26056e6ab024f67db6d17755a4980055e3ab814b
mran4564 <Madushan.Ranasinghe@sysco.com>
2023-12-07 17:32:55 +0530

    add packages, error handle middleware added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import config from './config/config';
-import axios from 'axios';
 import AuthRoutes from './route/auth.route';
 import { validateTokenMiddleware } from './middleware/auth.validator.cognito';
 import ProductRoutes from './route/products.route';
@@ -11,17 +10,26 @@ const app: Application = express();
 const authRoutes = new AuthRoutes();
 const productRoutes = new ProductRoutes();
 
-app.use(
-  cors({
-    credentials: true,
-  }),
-);
+const errorHandler: ErrorRequestHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error.response.data.message) {
+    res.status(error.response.status).json(error.response.data);
+  } else {
+    res.status(error.response.status).json(error.response.data);
+  }
+};
+
+const corsOptions = {
+  origin: ['http://localhost:4201', 'http://localhost:4200'],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes.getRouter());
+app.use('api/products', productRoutes.getRouter());
 app.use(validateTokenMiddleware);
 app.use('/api/products', productRoutes.getRouter());
-
+app.use(errorHandler);
 try {
   app.listen(config.port, (): void => {
     console.log(`Connected successfully on port ${config.port}`);
--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,7 @@ const config = {
   endpoint: process.env.API_URL,
   port: process.env.PORT,
   productApi: process.env.PRODUCTS_BASE_URL || '',
+  authApi: process.env.USER_BASE_URL || '',
   cognitoUserPoolId: process.env.AWS_USER_POOL_ID || '',
   cognitoClientId: process.env.AWS_COGNITO_CLIENT_ID || '',
   cognitoRegion: process.env.AWS_COGNITO_REGION || '',
