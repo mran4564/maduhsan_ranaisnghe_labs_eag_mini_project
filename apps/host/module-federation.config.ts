88a5fca58fe7e926bc83a035d70051bb7af641b3
mran4564 <Madushan.Ranasinghe@sysco.com>
2023-12-13 00:45:29 +0530

    (host): add role based routing

--- apps/host/module-federation.config.ts
+++ apps/host/module-federation.config.ts
@@ -2,7 +2,13 @@ import { ModuleFederationConfig } from '@nx/webpack';
 
 const config: ModuleFederationConfig = {
   name: 'host',
-  remotes: ['auth', 'global-store', 'supplier-portal'],
+  remotes: [
+    'auth',
+    'global-store',
+    'supplier-portal',
+    'shop',
+    'data-steward-portal',
+  ],
 };
 
 export default config;
--- apps/host/src/components/Dashboard.component.tsx
+++ apps/host/src/components/Dashboard.component.tsx
@@ -1,27 +1,82 @@
 import { Grid, GridItem } from '@chakra-ui/react';
 import Navbar from './NavBar.component';
 import React from 'react';
+import loadSession from '../utils/sessionData';
 
 const SupplierPortal = React.lazy(() => import('supplier-portal/Module'));
+const CustomerShop = React.lazy(() => import('shop/Module'));
+const DataSteward = React.lazy(() => import('data-steward-portal/Module'));
+
+export const UserRoleEnum = {
+  DATA_STEWARD: 'DATA_STEWARD',
+  CUSTOMER: 'CUSTOMER',
+  SUPPLIER: 'SUPPLIER',
+};
 
 const Dashboard = () => {
-  return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-      templateColumns={{
-        base: '1fr',
-        lg: '250px 1fr',
-      }}
-    >
-      <GridItem area="nav">
-        <Navbar />
-      </GridItem>
-      <SupplierPortal />
-    </Grid>
-  );
+  const { userRole } = loadSession();
+
+  if (userRole === UserRoleEnum.CUSTOMER) {
+    return (
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: '1fr',
+          lg: '250px 1fr',
+        }}
+      >
+        <GridItem area="nav">
+          <Navbar />
+        </GridItem>
+        <CustomerShop />
+      </Grid>
+    );
+  }
+
+  if (userRole === UserRoleEnum.SUPPLIER) {
+    return (
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: '1fr',
+          lg: '250px 1fr',
+        }}
+      >
+        <GridItem area="nav">
+          <Navbar />
+        </GridItem>
+        <SupplierPortal />
+      </Grid>
+    );
+  }
+
+  if (userRole === UserRoleEnum.DATA_STEWARD) {
+    return (
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: '1fr',
+          lg: '250px 1fr',
+        }}
+      >
+        <GridItem area="nav">
+          <Navbar />
+        </GridItem>
+        <DataSteward />
+      </Grid>
+    );
+  }
+
+  return <div>something went wrong! try again</div>;
 };
 
 export default Dashboard;
