64294d0caa3dc6f2ab00d25876c898c9b059b0a6
mran4564 <Madushan.Ranasinghe@sysco.com>
2023-12-18 12:48:42 +0530

    update category service filename

--- src/controller/category.controller.ts
+++ src/controller/category.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { CategoryCreateDto } from '../model/category.model';
-import CategoryService from '../service/categoryService';
+import CategoryService from '../service/category.service';
 
 const categoryService = new CategoryService();
 
